29774d086b0ce0d0711960cf1b4320a655aa2ce1
yi-xu-0100 <xjy123456120@163.com>
2017-07-21 11:24:37 +0800

    day2

---
+++ www/app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*- 
+""" 
+File Name: www/app.py
+Author: yi_Xu
+Created Time: 2017年07月21日 星期五 11时07分41秒
+""" 
+
+""" 
+project for python web app.
+""" 
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1>',content_type='text/html')
+
+@asyncio.coroutine
+def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
